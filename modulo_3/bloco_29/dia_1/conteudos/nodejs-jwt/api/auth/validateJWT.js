060d90dccba50a643947bba3c5804b4be8ea594e
Wesley Maia <maia.weol@gmail.com>
2022-01-15 17:33:27 -0300

    NodeJS - JWT - (JSON Web Token)

---
+++ modulo_3/bloco_29/dia_1/conteudos/nodejs-jwt/api/auth/validateJWT.js
@@ -0,0 +1,67 @@
+// validateJWT.js
+const jwt = require('jsonwebtoken');
+const model = require('../../models/User');
+
+/* Mesma chave privada que usamos para criptografar o token.
+   Agora, vamos usá-la para descriptografá-lo.
+   Numa aplicação real, essa chave jamais ficaria hardcoded no código assim,
+   e muitos menos de forma duplicada, mas aqui só estamos interessados em
+   ilustrar seu uso ;) */
+const segredo = 'seusecretdetoken';
+
+module.exports = async (req, res, next) => {
+  /* Aquele token gerado anteriormente virá na requisição através do
+     header Authorization em todas as rotas que queremos que
+     sejam autenticadas. */
+  const token = req.headers['authorization'];
+
+  /* Caso o token não seja informado, simplesmente retornamos
+     o código de status 401 - não autorizado. */
+  if (!token) {
+    return res.status(401).json({ error: 'Token não encontrado' });
+  }
+
+  try {
+    /* Através o método verify, podemos validar e decodificar o nosso JWT. */
+    const decoded = jwt.verify(token, segredo);
+    console.log("🚀 ~ file: validateJWT.js ~ line 27 ~ module.exports= ~ decoded", decoded)
+    /*
+      A variável decoded será um objeto equivalente ao seguinte:
+      {
+        data: {
+          _id: '5e54590ba49448f7e5fa73c0',
+          username: 'italssodj',
+          password: 'senha123'
+        },
+        iat: 1582587327,
+        exp: 1584774714908
+      }
+    */
+
+    /* Caso o token esteja expirado, a própria biblioteca irá retornar um erro,
+       por isso não é necessário fazer validação do tempo.
+       Caso esteja tudo certo, nós então buscamos o usuário na base para obter seus dados atualizados */
+    
+       // NÃO EXISTE A FUNÇÃO FINDUSER EM MODEL
+       const user = await model.findUser(decoded.data.username);
+
+
+    /* Não existe um usuário na nossa base com o id informado no token. */
+    if (!user) {
+      return res
+        .status(401)
+        .json({ message: 'Erro ao procurar usuário do token.' });
+    }
+
+    /* O usuário existe! Colocamos ele em um campo no objeto req.
+       Dessa forma, o usuário estará disponível para outros middlewares que
+       executem em sequência */
+    req.user = user;
+
+    /* Por fim, chamamos o próximo middleware que, no nosso caso,
+       é a própria callback da rota. */
+    next();
+  } catch (err) {
+    return res.status(401).json({ message: err.message });
+  }
+};
